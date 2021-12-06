ca109166a7064e15e88b8205cc6c5736dec3516c
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-12-06 17:10:07 +0500

    installing packeges

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+import express from "express"
+import config from "config"
+
+
+
+
+
+
+const app = express()
+const PORT = config.get('port') || 5000
+
+
+app.listen(5000, () =>{
+    console.log("this is app is running");  
+})
