eb70fc3392cf29104f769df7d69669724f53b29c
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-12-14 03:22:17 +0500

    add
    add

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -8,6 +8,8 @@ import {
   getUserSessionsHandler,
 } from "../controller/session.controller";
 import { createSessionSchema } from "../schema/session.schema";
+import { createProductSchema } from "../schema/product.schema";
+import { createProductHandler } from "../controller/product.controller";
 
 const routes = (app: Express) => {
   app.get("/check", (req: Request, res: Response) => {
@@ -25,7 +27,11 @@ const routes = (app: Express) => {
 
   app.delete("/api/sessions", requireUser, deleteSessionHandler);
 
-  app.post("/api/posts");
+  app.post(
+    "/api/posts",
+    [requireUser, validate(createProductSchema)],
+    createProductHandler
+  );
 };
 
 export default routes;
